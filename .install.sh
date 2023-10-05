e4c9401a57b91508789891214f9886e1719d597e
outragedline <81770118+outragedline@users.noreply.github.com>
2023-10-05 03:15:10 -0300

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -1,7 +1,4 @@
-pkg i neovim nodejs-lts python ripgrep fd lua54 clang  unzip tidy wget lldb
-
+pkg i neovim nodejs-lts python ripgrep fd lua54 clang  unzip tidy wget lldb ranger zsh lazygit stow github-cli
 pip install wheel pynvim 
 sudo npm i -g neovim
-# Other important applications
-yay -S ranger  zsh lazygit stow github-cli
 git clone https://github.com/zap-zsh/zap ~/.local/share/zap
